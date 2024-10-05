d1811fc09d992e52ace2d1b08aa3effb655e088d
francisvirginius <francisvirginius@gmail.com>
2024-10-05 11:44:36 +0200

    Initial Commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by francis virginius on 05/10/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
